3c7c4e7a762309d7001d2b0bf6cf003b1d36642d
Félix Gómez <felix.gomez.lopez@gmail.com>
2018-03-07 19:35:33 +0100

    first service implementation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,59 +6,26 @@ use App\Service\TwitterClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
 class DefaultController extends Controller
 {
 
     /**
-     * @Route("/injection")
+     * @Route("/service")
+     * @param TwitterClient $twitterClient
+     * @return Response
      */
     public function test_injection(TwitterClient $twitterClient)
     {
-        return new Response($twitterClient->sayHello());
-    }
 
-    /**
-     * @Route("/oauth", name="twitter_oauth")
-     */
-    public function index()
-    {
+        $twitterResult = $twitterClient->findTweetsWith('#farina');
 
-        $stack = HandlerStack::create();
-
-        // Change oAuth credentials with yours
-        $middleware = new Oauth1([
-            'consumer_key' => 'my_key',
-            'consumer_secret' => 'my_secret',
-            'token' => 'my_token',
-            'token_secret' => 'my_token_secret'
-        ]);
-
-        $stack->push($middleware);
-
-        $client = new Client([
-            'base_uri' => 'https://api.twitter.com/1.1/',
-            'handler' => $stack
-        ]);
-
-        // Set the "auth" request option to "oauth" to sign using oauth
-
-        $res = $client->get('search/tweets.json', [
-            'auth' => 'oauth',
-            'query' => [
-                'q' => urlencode('#fariña'),
-                'include_entities' => false,
-                'result_type' => 'recent',
-                'count' => 4
-            ]
-        ]);
+        dump($twitterResult);
 
         return $this->render('default/index.html.twig', [
-            'response' => json_decode($res->getBody()->getContents())
+            'response' => $twitterResult
         ]);
 
     }
+
 }
--- src/Service/TwitterClient.php
+++ src/Service/TwitterClient.php
@@ -4,11 +4,78 @@
 namespace App\Service;
 
 
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Client;
+
 class TwitterClient
 {
-    public function sayHello()
+
+    private $client;
+
+    /**
+     * TwitterClient constructor.
+     *
+     * @param $base_uri
+     * @param $consumer_key
+     * @param $consumer_secret
+     * @param $token
+     * @param $token_secret
+     */
+    public function __construct($base_uri, $consumer_key, $consumer_secret, $token, $token_secret)
     {
-        return "Hello";
+
+        $stack = HandlerStack::create();
+
+        $middleware = new Oauth1([
+            'consumer_key' => $consumer_key,
+            'consumer_secret' => $consumer_secret,
+            'token' => $token,
+            'token_secret' => $token_secret
+        ]);
+        $stack->push($middleware);
+
+
+        $this->client = new Client([
+            'base_uri' => $base_uri,
+            'handler' => $stack
+        ]);
+    }
+
+    /**
+     * Find tweets by text
+     *
+     * @param $text
+     * @param bool $include_entities
+     * @param string $result_type
+     * @param int $count
+     * @return array
+     */
+    public function findTweetsWith(
+        $text,
+        $include_entities = false,
+        $result_type = 'recent',
+        $count = 4
+    )
+    {
+
+        try {
+            $JSONResponse = $this->client->get('search/tweets.json', [
+                'auth' => 'oauth',
+                'query' => [
+                    'q' => $text,
+                    'include_entities' => $include_entities,
+                    'result_type' => $result_type,
+                    'count' => $count
+                ]
+            ])->getBody()->getContents();
+
+            return json_decode($JSONResponse);
+
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+
     }
 
 }
