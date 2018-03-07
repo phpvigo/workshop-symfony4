822e674a1dfcdcba2c9f8cd41c7bb13cf93d6e6d
Félix Gómez <felix.gomez.lopez@gmail.com>
2018-03-07 19:43:54 +0100

    created Tweet entity and associated repository

---
+++ src/Entity/Tweet.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TweetRepository")
+ */
+class Tweet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    // add your own fields
+}
