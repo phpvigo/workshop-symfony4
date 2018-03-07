97c94c9fb404d1bc9514738532d4e141cba97d97
Félix Gómez <felix.gomez.lopez@gmail.com>
2018-03-07 19:47:07 +0100

    final Tweet entity

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,30 +2,10 @@
 
 namespace App\Controller;
 
-use App\Service\TwitterClient;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-
-    /**
-     * @Route("/service")
-     * @param TwitterClient $twitterClient
-     * @return Response
-     */
-    public function test_injection(TwitterClient $twitterClient)
-    {
-
-        $twitterResult = $twitterClient->findTweetsWith('#farina');
-
-        dump($twitterResult);
-
-        return $this->render('default/index.html.twig', [
-            'response' => $twitterResult
-        ]);
-
-    }
 
 }
--- src/Entity/Tweet.php
+++ src/Entity/Tweet.php
@@ -16,5 +16,127 @@ class Tweet
      */
     private $id;
 
-    // add your own fields
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $tweetId;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $userName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $userImage;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTweetId()
+    {
+        return $this->tweetId;
+    }
+
+    /**
+     * @param mixed $tweetId
+     * @return Tweet
+     */
+    public function setTweetId($tweetId)
+    {
+        $this->tweetId = $tweetId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * @param mixed $userName
+     * @return Tweet
+     */
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserImage()
+    {
+        return $this->userImage;
+    }
+
+    /**
+     * @param mixed $userImage
+     * @return Tweet
+     */
+    public function setUserImage($userImage)
+    {
+        $this->userImage = $userImage;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     * @return Tweet
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     * @return Tweet
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
 }
