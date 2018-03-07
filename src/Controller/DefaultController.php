e2ff7d352234cd64875260bb865dab06a08e30a1
Félix Gómez <felix.gomez.lopez@gmail.com>
2018-03-07 19:11:06 +0100

    testing and debugging twitter API in controller (bad!)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,50 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/oauth", name="twitter_oauth")
      */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+
+        $stack = HandlerStack::create();
+
+        // Change oAuth credentials with yours
+        $middleware = new Oauth1([
+            'consumer_key' => 'my_key',
+            'consumer_secret' => 'my_secret',
+            'token' => 'my_token',
+            'token_secret' => 'my_token_secret'
         ]);
+
+        $stack->push($middleware);
+
+        $client = new Client([
+            'base_uri' => 'https://api.twitter.com/1.1/',
+            'handler' => $stack
+        ]);
+
+        // Set the "auth" request option to "oauth" to sign using oauth
+
+        $res = $client->get('search/tweets.json', [
+            'auth' => 'oauth',
+            'query' => [
+                'q' => urlencode('#fariña'),
+                'include_entities' => false,
+                'result_type' => 'recent',
+                'count' => 4
+            ]
+        ]);
+
+        return $this->render('default/index.html.twig', [
+            'response' => json_decode($res->getBody()->getContents())
+        ]);
+
     }
 }
