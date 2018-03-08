3ea47866d353fa85d5d01297d476eb1356fce16a
Félix Gómez <felix.gomez.lopez@gmail.com>
2018-03-08 10:57:54 +0100

    modified frontend to shuffle tweets on refresh

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,8 +4,30 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Tweet;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/", name="random_tweet")
+     * @param EntityManagerInterface $entityManager
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getRandomTweet(EntityManagerInterface $entityManager)
+    {
+
+        $tweets = $entityManager->getRepository(Tweet::class)->findAll();
+
+        shuffle($tweets);
+
+        $winner = $tweets[0];
+
+        return $this->render('default/index.html.twig', [
+            'winner' => $winner
+        ]);
+
+    }
 
 }
