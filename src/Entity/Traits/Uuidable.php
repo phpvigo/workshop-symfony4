bd06592cdec6c23a7358b79695d039ad8c388932
Rolando Caldas <rolando.caldas@gmail.com>
2018-06-13 14:28:28 +0200

    Trait to add uuid to the entities

---
+++ src/Entity/Traits/Uuidable.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rolando.caldas
+ * Date: 13/06/2018
+ * Time: 13:39
+ */
+
+namespace App\Entity\Traits;
+
+use Ramsey\Uuid\Uuid;
+
+trait Uuidable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+
+    private function generateId()
+    {
+        $this->id = Uuid::uuid4()->toString();
+    }
+
+    public function getId() : Uuid
+    {
+        return $this->id;
+    }
+}
