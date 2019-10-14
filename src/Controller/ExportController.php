4a96043968919f4287445815c46941570bc7c7dd
Rolando Caldas <rolando.caldas@gmail.com>
2019-10-15 01:41:23 +0200

    clean the controller code

--- src/Controller/ExportController.php
+++ src/Controller/ExportController.php
@@ -30,82 +30,111 @@ class ExportController
 
         switch ($type) {
             case "json":
-                $response = new Response();
-                $response->headers->set('Content-Type', 'application/json');
-                $data = [];
-                foreach ($tweets AS $tweet) {
-                    $data[] = [
-                        'id' => $tweet->getTweetId(),
-                        'username' => $tweet->getUserName(),
-                        'user_image' => $tweet->getUserImage(),
-                        'content' => $tweet->getContent(),
-                        'link' => 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(),
-                        'date' => $tweet->getCreatedAt()->format('c')
-                    ];
-                }
-
-                $response->setContent(json_encode($data));
-                return $response;
+                return $this->responseJson($tweets);
                 break;
 
             case "csv":
-                $response = new Response();
-
-                $csvTempFile = fopen('php://memory', 'r+');
-                fputcsv($csvTempFile, ['id', 'username', 'user_image', 'content', 'link', 'date']);
-                foreach ($tweets AS $tweet) {
-                    fputcsv($csvTempFile, [$tweet->getTweetId(), $tweet->getUserName(), $tweet->getUserImage(), $tweet->getContent(), 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(), $tweet->getCreatedAt()->format('c')]);
-                }
-                rewind($csvTempFile);
-                $csv_line = stream_get_contents($csvTempFile);
-                fclose($csvTempFile);
-                $response->setContent($csv_line);
-                return $response;
+                return $this->responseCsv($tweets);
                 break;
 
 
             case "excel":
-                $spreadsheet = new Spreadsheet();
-                $spreadsheet->removeSheetByIndex(0);
-                $worksheet = new Worksheet();
-                $spreadsheet->addSheet($worksheet);
-                $i = 1;
-                $worksheet->setCellValue('A' . $i, 'id');
-                $worksheet->setCellValue('B' . $i, 'username');
-                $worksheet->setCellValue('C' . $i, 'user_image');
-                $worksheet->setCellValue('D' . $i, 'content');
-                $worksheet->setCellValue('E' . $i, 'link');
-                $worksheet->setCellValue('F' . $i, 'date');
-                $i++;
-                foreach ($tweets AS $tweet) {
-                    $worksheet->setCellValue('A' . $i, $tweet->getTweetId());
-                    $worksheet->setCellValue('B' . $i, $tweet->getUserName());
-                    $worksheet->setCellValue('C' . $i, $tweet->getUserImage());
-                    $worksheet->setCellValue('D' . $i, $tweet->getContent());
-                    $worksheet->setCellValue('E' . $i, 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId());
-                    $worksheet->setCellValue('F' . $i, $tweet->getCreatedAt()->format('c'));
-
-                    $worksheet->getColumnDimension('A')->setAutoSize(true);
-                    $worksheet->getColumnDimension('B')->setAutoSize(true);
-                    $worksheet->getColumnDimension('C')->setAutoSize(true);
-                    $worksheet->getColumnDimension('D')->setAutoSize(true);
-                    $worksheet->getColumnDimension('E')->setAutoSize(true);
-                    $worksheet->getColumnDimension('F')->setAutoSize(true);
-                    $i++;
-                }
-                $response = new StreamedResponse();
-                $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-                $response->headers->set('Content-Disposition', 'attachment; filename="hashtag-' . (new \DateTimeImmutable())->format('YmdHis') . '.xlsx"');
-                $response->setCallback(static function () use ($spreadsheet) {
-                    $writer = new Xlsx($spreadsheet);
-                    $writer->save('php://output');
-                });
-
-                return $response->send();
+                return $this->responseExcel($tweets, $hashtag);
                 break;
         }
 
 
         return (new Response());
+    }
+
+    /**
+     * @param array $tweets
+     * @return Response
+     */
+    private function responseJson(array $tweets): Response
+    {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $data = [];
+        foreach ($tweets AS $tweet) {
+            $data[] = [
+                'id' => $tweet->getTweetId(),
+                'username' => $tweet->getUserName(),
+                'user_image' => $tweet->getUserImage(),
+                'content' => $tweet->getContent(),
+                'link' => 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(),
+                'date' => $tweet->getCreatedAt()->format('c')
+            ];
+        }
+
+        $response->setContent(json_encode($data));
+        return $response;
+    }
+
+    /**
+     * @param array $tweets
+     * @return Response
+     */
+    private function responseCsv(array $tweets): Response
+    {
+        $response = new Response();
+
+        $csvTempFile = fopen('php://memory', 'r+');
+        fputcsv($csvTempFile, ['id', 'username', 'user_image', 'content', 'link', 'date']);
+        foreach ($tweets AS $tweet) {
+            fputcsv($csvTempFile, [$tweet->getTweetId(), $tweet->getUserName(), $tweet->getUserImage(), $tweet->getContent(), 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(), $tweet->getCreatedAt()->format('c')]);
+        }
+        rewind($csvTempFile);
+        $csv_line = stream_get_contents($csvTempFile);
+        fclose($csvTempFile);
+        $response->setContent($csv_line);
+        return $response;
+    }
+
+    /**
+     * @param array $tweets
+     * @param \App\Entity\Hashtag|null $hashtag
+     * @return StreamedResponse
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    private function responseExcel(array $tweets, ?\App\Entity\Hashtag $hashtag): ExportController
+    {
+        $spreadsheet = new Spreadsheet();
+        $spreadsheet->removeSheetByIndex(0);
+        $worksheet = new Worksheet();
+        $spreadsheet->addSheet($worksheet);
+        $i = 1;
+        $worksheet->setCellValue('A' . $i, 'id');
+        $worksheet->setCellValue('B' . $i, 'username');
+        $worksheet->setCellValue('C' . $i, 'user_image');
+        $worksheet->setCellValue('D' . $i, 'content');
+        $worksheet->setCellValue('E' . $i, 'link');
+        $worksheet->setCellValue('F' . $i, 'date');
+        $i++;
+        foreach ($tweets AS $tweet) {
+            $worksheet->setCellValue('A' . $i, $tweet->getTweetId());
+            $worksheet->setCellValue('B' . $i, $tweet->getUserName());
+            $worksheet->setCellValue('C' . $i, $tweet->getUserImage());
+            $worksheet->setCellValue('D' . $i, $tweet->getContent());
+            $worksheet->setCellValue('E' . $i, 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId());
+            $worksheet->setCellValue('F' . $i, $tweet->getCreatedAt()->format('c'));
+
+            $worksheet->getColumnDimension('A')->setAutoSize(true);
+            $worksheet->getColumnDimension('B')->setAutoSize(true);
+            $worksheet->getColumnDimension('C')->setAutoSize(true);
+            $worksheet->getColumnDimension('D')->setAutoSize(true);
+            $worksheet->getColumnDimension('E')->setAutoSize(true);
+            $worksheet->getColumnDimension('F')->setAutoSize(true);
+            $i++;
+        }
+        $response = new StreamedResponse();
+        $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        $response->headers->set('Content-Disposition', 'attachment; filename="hashtag-' . (new \DateTimeImmutable())->format('YmdHis') . '.xlsx"');
+        $response->setCallback(static function () use ($spreadsheet) {
+            $writer = new Xlsx($spreadsheet);
+            $writer->save('php://output');
+        });
+
+        return $response->send();
     }
 }
