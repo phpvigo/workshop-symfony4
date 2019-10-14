e2d474c71520ca4ee18b8d43ed98fd109f22cc59
Rolando Caldas <rolando.caldas@gmail.com>
2019-10-15 01:44:02 +0200

    [S] move transform logic into a UseCase

--- src/Controller/ExportController.php
+++ src/Controller/ExportController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\HashtagRepository;
 use App\Repository\TweetRepository;
+use App\UseCase\TransformTweetsOfHashtagIntoFormat;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
@@ -32,105 +33,24 @@ class ExportController
         switch ($type) {
             case "json":
                 $response->headers->set('Content-Type', 'application/json');
-                $response->setContent($this->responseJson($tweets));
+                $response->setContent((new TransformTweetsOfHashtagIntoFormat())->dispatch($hashtag, $tweets, $type));
                 break;
 
             case "csv":
-                $response->setContent($this->responseCsv($tweets));
+                $response->setContent((new TransformTweetsOfHashtagIntoFormat())->dispatch($hashtag, $tweets, $type));
                 break;
 
             case "excel":
                 $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
                 $response->headers->set('Content-Disposition', 'attachment; filename="hashtag-' . (new \DateTimeImmutable())->format('YmdHis') . '.xlsx"');
                 $response->sendHeaders();
-                $response->setContent($this->responseExcel($tweets, $hashtag));
+                $response->setContent((new TransformTweetsOfHashtagIntoFormat())->dispatch($hashtag, $tweets, $type));
                 break;
         }
         return $response;
     }
 
-    /**
-     * @param array $tweets
-     * @return string
-     */
-    private function responseJson(array $tweets): string
-    {
-        $data = [];
-        foreach ($tweets AS $tweet) {
-            $data[] = [
-                'id' => $tweet->getTweetId(),
-                'username' => $tweet->getUserName(),
-                'user_image' => $tweet->getUserImage(),
-                'content' => $tweet->getContent(),
-                'link' => 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(),
-                'date' => $tweet->getCreatedAt()->format('c')
-            ];
-        }
 
-        return json_encode($data);
-    }
-
-    /**
-     * @param array $tweets
-     * @return string
-     */
-    private function responseCsv(array $tweets): string
-    {
-        $csvTempFile = fopen('php://memory', 'r+');
-        fputcsv($csvTempFile, ['id', 'username', 'user_image', 'content', 'link', 'date']);
-        foreach ($tweets AS $tweet) {
-            fputcsv($csvTempFile, [$tweet->getTweetId(), $tweet->getUserName(), $tweet->getUserImage(), $tweet->getContent(), 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(), $tweet->getCreatedAt()->format('c')]);
-        }
-        rewind($csvTempFile);
-        $csv_line = stream_get_contents($csvTempFile);
-        fclose($csvTempFile);
-        return $csv_line;
-    }
-
-    /**
-     * @param array $tweets
-     * @param \App\Entity\Hashtag|null $hashtag
-     * @return string
-     * @throws \PhpOffice\PhpSpreadsheet\Exception
-     */
-    private function responseExcel(array $tweets, ?\App\Entity\Hashtag $hashtag): string
-    {
-        $spreadsheet = new Spreadsheet();
-        $spreadsheet->removeSheetByIndex(0);
-        $worksheet = new Worksheet();
-        $spreadsheet->addSheet($worksheet);
-        $i = 1;
-        $worksheet->setCellValue('A' . $i, 'id');
-        $worksheet->setCellValue('B' . $i, 'username');
-        $worksheet->setCellValue('C' . $i, 'user_image');
-        $worksheet->setCellValue('D' . $i, 'content');
-        $worksheet->setCellValue('E' . $i, 'link');
-        $worksheet->setCellValue('F' . $i, 'date');
-        $i++;
-        foreach ($tweets AS $tweet) {
-            $worksheet->setCellValue('A' . $i, $tweet->getTweetId());
-            $worksheet->setCellValue('B' . $i, $tweet->getUserName());
-            $worksheet->setCellValue('C' . $i, $tweet->getUserImage());
-            $worksheet->setCellValue('D' . $i, $tweet->getContent());
-            $worksheet->setCellValue('E' . $i, 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId());
-            $worksheet->setCellValue('F' . $i, $tweet->getCreatedAt()->format('c'));
-
-            $worksheet->getColumnDimension('A')->setAutoSize(true);
-            $worksheet->getColumnDimension('B')->setAutoSize(true);
-            $worksheet->getColumnDimension('C')->setAutoSize(true);
-            $worksheet->getColumnDimension('D')->setAutoSize(true);
-            $worksheet->getColumnDimension('E')->setAutoSize(true);
-            $worksheet->getColumnDimension('F')->setAutoSize(true);
-            $i++;
-        }
-
-        ob_start();
-        $writer = new Xlsx($spreadsheet);
-        $writer->save('php://output');
-        $content = ob_get_flush();
-
-        return $content;
-    }
 }
 
 
---
+++ src/UseCase/TransformTweetsOfHashtagIntoFormat.php
@@ -0,0 +1,114 @@
+<?php
+
+
+namespace App\UseCase;
+
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+class TransformTweetsOfHashtagIntoFormat
+{
+    public function dispatch($hashtag, $tweets, $type) : string
+    {
+        $return = null;
+        switch ($type) {
+            case "json":
+                $return = $this->responseJson($tweets);
+                break;
+
+            case "csv":
+                $return = $this->responseCsv($tweets);
+                break;
+
+            case "excel":
+                $return = $this->responseExcel($tweets, $hashtag);
+                break;
+        }
+        return $return;
+    }
+
+    /**
+     * @param array $tweets
+     * @return string
+     */
+    private function responseJson(array $tweets): string
+    {
+        $data = [];
+        foreach ($tweets AS $tweet) {
+            $data[] = [
+                'id' => $tweet->getTweetId(),
+                'username' => $tweet->getUserName(),
+                'user_image' => $tweet->getUserImage(),
+                'content' => $tweet->getContent(),
+                'link' => 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(),
+                'date' => $tweet->getCreatedAt()->format('c')
+            ];
+        }
+
+        return json_encode($data);
+    }
+
+    /**
+     * @param array $tweets
+     * @return string
+     */
+    private function responseCsv(array $tweets): string
+    {
+        $csvTempFile = fopen('php://memory', 'r+');
+        fputcsv($csvTempFile, ['id', 'username', 'user_image', 'content', 'link', 'date']);
+        foreach ($tweets AS $tweet) {
+            fputcsv($csvTempFile, [$tweet->getTweetId(), $tweet->getUserName(), $tweet->getUserImage(), $tweet->getContent(), 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(), $tweet->getCreatedAt()->format('c')]);
+        }
+        rewind($csvTempFile);
+        $csv_line = stream_get_contents($csvTempFile);
+        fclose($csvTempFile);
+        return $csv_line;
+    }
+
+    /**
+     * @param array $tweets
+     * @param \App\Entity\Hashtag|null $hashtag
+     * @return string
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    private function responseExcel(array $tweets, ?\App\Entity\Hashtag $hashtag): string
+    {
+        $spreadsheet = new Spreadsheet();
+        $spreadsheet->removeSheetByIndex(0);
+        $worksheet = new Worksheet();
+        $spreadsheet->addSheet($worksheet);
+        $i = 1;
+        $worksheet->setCellValue('A' . $i, 'id');
+        $worksheet->setCellValue('B' . $i, 'username');
+        $worksheet->setCellValue('C' . $i, 'user_image');
+        $worksheet->setCellValue('D' . $i, 'content');
+        $worksheet->setCellValue('E' . $i, 'link');
+        $worksheet->setCellValue('F' . $i, 'date');
+        $i++;
+        foreach ($tweets AS $tweet) {
+            $worksheet->setCellValue('A' . $i, $tweet->getTweetId());
+            $worksheet->setCellValue('B' . $i, $tweet->getUserName());
+            $worksheet->setCellValue('C' . $i, $tweet->getUserImage());
+            $worksheet->setCellValue('D' . $i, $tweet->getContent());
+            $worksheet->setCellValue('E' . $i, 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId());
+            $worksheet->setCellValue('F' . $i, $tweet->getCreatedAt()->format('c'));
+
+            $worksheet->getColumnDimension('A')->setAutoSize(true);
+            $worksheet->getColumnDimension('B')->setAutoSize(true);
+            $worksheet->getColumnDimension('C')->setAutoSize(true);
+            $worksheet->getColumnDimension('D')->setAutoSize(true);
+            $worksheet->getColumnDimension('E')->setAutoSize(true);
+            $worksheet->getColumnDimension('F')->setAutoSize(true);
+            $i++;
+        }
+
+        ob_start();
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('php://output');
+        $content = ob_get_flush();
+
+        return $content;
+    }
+}
