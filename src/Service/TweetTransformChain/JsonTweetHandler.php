3cd7e3036f0073041efe0554d32276b9fa8c61e6
Rolando Caldas <rolando.caldas@gmail.com>
2019-10-15 01:52:14 +0200

    [OI] New Chain Of Responsability to format the tweets

---
+++ src/Service/TweetTransformChain/JsonTweetHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Service\TweetTransformChain;
+
+
+use App\Entity\TweetCollection;
+
+class JsonTweetHandler extends AbstractTweetTransform
+{
+    public function handle(TweetTransformRequest $tweetTransformRequest): string
+    {
+        if ($tweetTransformRequest->type() !== 'json') {
+            return parent::handle($tweetTransformRequest);
+        }
+
+        return $this->processResponse($tweetTransformRequest->tweetCollection());
+    }
+
+    /**
+     * @param TweetCollection $tweets
+     * @return string
+     */
+    private function processResponse(TweetCollection $tweets): string
+    {
+        $data = [];
+        foreach ($tweets AS $tweet) {
+            $data[] = [
+                'id' => $tweet->getTweetId(),
+                'username' => $tweet->getUserName(),
+                'user_image' => $tweet->getUserImage(),
+                'content' => $tweet->getContent(),
+                'link' => 'https://twitter.com/' . $tweet->getUserName() . '/status/' . $tweet->getTweetId(),
+                'date' => $tweet->getCreatedAt()->format('c')
+            ];
+        }
+
+        return json_encode($data);
+    }
+}
