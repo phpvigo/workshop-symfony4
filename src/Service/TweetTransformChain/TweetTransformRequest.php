7171b175932fb84e98b92f0a66e4deac8fcd361d
Rolando Caldas <rolando.caldas@gmail.com>
2019-10-15 01:51:36 +0200

    [Clean Code] Extract params into class (VO)

---
+++ src/Service/TweetTransformChain/TweetTransformRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service\TweetTransformChain;
+
+use App\Entity\Hashtag;
+use App\Entity\TweetCollection;
+
+class TweetTransformRequest
+{
+    /**
+     * @var string
+     */
+    private $type;
+    /**
+     * @var Hashtag
+     */
+    private $hashtag;
+    /**
+     * @var TweetCollection
+     */
+    private $tweetCollection;
+
+    public function __construct(string $type, Hashtag $hashtag, TweetCollection $tweetCollection)
+    {
+        $this->type = $type;
+        $this->hashtag = $hashtag;
+        $this->tweetCollection = $tweetCollection;
+    }
+
+    /**
+     * @return string
+     */
+    public function type(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return Hashtag
+     */
+    public function hashtag(): Hashtag
+    {
+        return $this->hashtag;
+    }
+
+    /**
+     * @return TweetCollection
+     */
+    public function tweetCollection(): TweetCollection
+    {
+        return $this->tweetCollection;
+    }
+
+
+}
